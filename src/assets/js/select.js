33679ebfa2acd9a797d2eedd37f3bb09cc75c22d
viridius-hub <smiledie-hub@yandex.ru>
2024-04-15 13:01:26 +0500

    add adaptive catalog_item_select

--- src/assets/js/select.js
+++ src/assets/js/select.js
@@ -4,19 +4,29 @@ for (let i = 0; i < fieldsSelects.length; i++) {
     const fieldEl = fieldsSelects[i]
     const fieldOptionsWrapper = fieldEl.parentElement.querySelector('[data-select-options]')
 
-    console.log('fieldOptionsWrapper', fieldOptionsWrapper)
-
     if (fieldOptionsWrapper) {
         let isOpen = false
-        
-        fieldEl.addEventListener('click', (e)=>{
+
+        const options = fieldOptionsWrapper.querySelectorAll('[data-select-option]')
+
+        options.forEach(option => {
+            option.addEventListener('click', (e) => {
+                fieldEl.value = e.currentTarget.textContent
+                fieldEl.classList.remove('select__input_active')
+            })
+        })
+
+        fieldEl.setAttribute('readonly', "true")
+
+        fieldEl.addEventListener('click', (e) => {
             e.preventDefault()
-            isOpen=!isOpen
-            fieldOptionsWrapper.style.display = isOpen? 'flex' : 'none'
+            isOpen = !isOpen
+            fieldOptionsWrapper.style.display = isOpen ? 'flex' : 'none'
             if (isOpen) {
                 fieldEl.classList.add('select__input_active')
+            } else {
+                fieldEl.classList.remove('select__input_active')
             }
-            else fieldEl.classList.remove('select__input_active')
         })
     }
 }
